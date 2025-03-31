32c52ae3657d35c290bee7e6e0354f2c0d7fc1da
Bheem-Works <shokokomichan098@gmail.com>
2025-03-31 10:17:44 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import Features from './components/Features.jsx'
+import WorkFlow from './components/WorkFlow.jsx'
+import Pricing from './components/Pricing.jsx'
+import Testimonial from './components/Testimonial.jsx'
+import Footer from './components/Footer.jsx'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection /> 
+        <Features /> 
+        <WorkFlow />
+        <Pricing />
+        <Testimonial />
+        <Footer />
+      </div>
+    </>
+
+  )
+}
+
+export default App
